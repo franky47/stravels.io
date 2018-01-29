8381dc20467af49b1f3c46edd39c7a00ce4eaf6c
Francois Best <francoisbest@gmail.com>
2018-01-29 22:04:44 +0100

    feat: Skip landing when authenticated

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+import auth from '../lib/auth'
 
-export default () => (
-  <React.Fragment>
-    <h1>Landing</h1>
-    <Link to='/travels'>My travels</Link>
-  </React.Fragment>
-)
+export default () => {
+  if (auth.isAuthenticated) {
+    return <Redirect to='/travels' />
+  }
+  return (
+    <React.Fragment>
+      <h1>Landing</h1>
+      <Link to='/travels'>My travels</Link>
+    </React.Fragment>
+  )
+}
