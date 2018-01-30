956038649454193228ae17c0ae8e098c40ab66bf
Francois Best <francoisbest@gmail.com>
2018-01-30 14:06:54 +0100

    feat: Styling of the session header

--- src/components/SessionHeader.js
+++ src/components/SessionHeader.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import auth from '../lib/auth'
+
+import './SessionHeader.css'
 
 class SessionHeader extends React.Component {
   render () {
@@ -13,25 +15,30 @@ class SessionHeader extends React.Component {
       return null
     }
     return (
-      <React.Fragment>
-        <h1>Hello, {meQuery.me.name}</h1>
-        <button onClick={this._logout}>Log out</button>
-      </React.Fragment>
+      <header className='session'>
+        <Link to='/profile'>
+          <img
+            src={meQuery.me.profilePicture}
+            alt={`Profile for ${meQuery.me.name}`}
+            className='profilePicture'
+          />
+        </Link>
+      </header>
     )
   }
-
-  _logout = () => {
-    // todo: https://www.apollographql.com/docs/react/recipes/authentication.html#login-logout
-    const { history } = this.props
-    auth.logout()
-      .then(() => history.push('/'))
-  }
+  // _logout = () => {
+  //   // todo: https://www.apollographql.com/docs/react/recipes/authentication.html#login-logout
+  //   const { history } = this.props
+  //   auth.logout()
+  //     .then(() => history.push('/'))
+  // }
 }
 
 const ME_QUERY = gql`
 query {
   me {
     name
+    profilePicture
   }
 }
 `
