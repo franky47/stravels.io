0ba9519b96ebd95e8878f0e9a5a34bf12cb9d117
Francois Best <francoisbest@gmail.com>
2018-01-30 09:48:24 +0100

    feat: Add GraphQL bindings

--- src/components/SessionHeader.js
+++ src/components/SessionHeader.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 import auth from '../lib/auth'
 
 class SessionHeader extends React.Component {
   render () {
     if (!auth.isAuthenticated) return null
 
-    const { name } = this.props
+    const { meQuery } = this.props
+    if (meQuery.loading) {
+      return null
+    }
     return (
       <React.Fragment>
-        <h1>Hello, {name}</h1>
+        <h1>Hello, {meQuery.me.name}</h1>
         <button onClick={this._logout}>Log out</button>
       </React.Fragment>
     )
   }
 
   _logout = () => {
+    // todo: https://www.apollographql.com/docs/react/recipes/authentication.html#login-logout
     const { history } = this.props
     auth.logout()
       .then(() => history.push('/'))
   }
 }
 
-export default withRouter(SessionHeader)
+const ME_QUERY = gql`
+query {
+  me {
+    name
+  }
+}
+`
+
+export default graphql(ME_QUERY, { name: 'meQuery' })(withRouter(SessionHeader))
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import auth from './lib/auth'
 
 // Root styling
 import 'normalize.css'
@@ -8,5 +14,29 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const httpLink = new HttpLink({
+  uri: 'https://xnm7m1xxml.lp.gql.zone/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: auth.token ? `Bearer ${auth.token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
