5ed8bcd5cfaced70ca5bed2dddb7c61ca58eec52
Francois Best <francoisbest@gmail.com>
2018-01-31 09:01:51 +0100

    feat: Bind data to list of travels and travel info

--- src/screens/Travel.js
+++ src/screens/Travel.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 import SessionHeader from '../components/SessionHeader'
 
-export default ({ match }) => (
-  <React.Fragment>
-    <SessionHeader />
-    <h1>Travel {match.params.id}</h1>
-  </React.Fragment>
-)
+class Travel extends React.Component {
+  render () {
+    const { data } = this.props
+    if (data.loading) return null
+    return (
+      <React.Fragment>
+        <SessionHeader />
+        <h2>{data.travel.name}</h2>
+        <ul>
+          { data.travel.activities.map(this._renderActivity) }
+        </ul>
+      </React.Fragment>
+    )
+  }
+
+  _renderActivity = (item) => {
+    return (
+      <li key={item.id}>
+        {item.name}
+      </li>
+    )
+  }
+}
+
+const query = gql`
+query Travel($id: ID!) {
+  travel(id: $id) {
+    name
+    activities {
+      id
+      name
+    }
+  }
+}
+`
+
+export default graphql(query, {
+  options: ({ match }) => ({
+    variables: {
+      id: match.params.id
+    }
+  })
+})(withRouter(Travel))
--- src/screens/TravelsList.js
+++ src/screens/TravelsList.js
@@ -1,16 +1,47 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
 import SessionHeader from '../components/SessionHeader'
 
-export default ({ match }) => (
-  <React.Fragment>
-    <SessionHeader />
-    <h1>My Travels</h1>
-    <ul>
-      <li><Link to={`${match.url}/foo`}>Foo</Link></li>
-      <li><Link to={`${match.url}/bar`}>Bar</Link></li>
-      <li><Link to={`${match.url}/egg`}>Egg</Link></li>
-      <li><Link to={`${match.url}/spam`}>Spam</Link></li>
-    </ul>
-  </React.Fragment>
-)
+class TravelsList extends React.Component {
+  render () {
+    const { data } = this.props
+    return (
+      <React.Fragment>
+        <SessionHeader />
+        <h1>My Travels</h1>
+        <ul>
+          { !data.loading &&
+            data.me.travels.map(this._renderItem)
+          }
+        </ul>
+      </React.Fragment>
+    )
+  }
+
+  _renderItem = (item) => {
+    const { match } = this.props
+    return (
+      <li key={item.id}>
+        <Link to={`${match.url}/${item.id}`} >
+          {item.name}
+        </Link>
+      </li>
+    )
+  }
+}
+
+const query = gql`
+query {
+  me {
+    travels {
+      id
+      name
+    }
+  }
+}
+`
+
+export default graphql(query)(withRouter(TravelsList))
