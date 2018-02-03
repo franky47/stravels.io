e5c5ad4fb3740c27fdb2aeb67f6d25302e5b0ea7
Francois Best <francoisbest@gmail.com>
2018-02-03 01:25:06 +0100

    feat: Add map displaying segments of a travel

--- src/screens/Travel.js
+++ src/screens/Travel.js
@@ -3,11 +3,13 @@ import { withRouter } from 'react-router-dom'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import SessionHeader from '../components/SessionHeader'
+import Map from '../components/Map'
 
 class Travel extends React.Component {
   render () {
     const { data } = this.props
     if (data.loading) return null
+    const revActivities = [...data.travel.activities].reverse()
     return (
       <React.Fragment>
         <SessionHeader />
@@ -15,6 +17,10 @@ class Travel extends React.Component {
         <ul>
           { data.travel.activities.map(this._renderActivity) }
         </ul>
+        <Map
+          polylines={revActivities.map(this._formatPolyline)}
+          colorBase={this._generateBaseColor()}
+        />
       </React.Fragment>
     )
   }
@@ -26,6 +32,11 @@ class Travel extends React.Component {
       </li>
     )
   }
+  _formatPolyline = ({ id, polyline }) => ({ id, encoded: polyline })
+  _generateBaseColor = () => this.props.data.travel.name
+    .split('')
+    .map(c => c.charCodeAt(0))
+    .reduce((acc, c) => acc % 2 ? acc * c : acc + c) % 360
 }
 
 const query = gql`
@@ -35,6 +46,7 @@ query Travel($id: ID!) {
     activities {
       id
       name
+      polyline
     }
   }
 }
