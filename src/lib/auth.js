dd758be54b43cde2f5937cec8fe4a1900921b356
Francois Best <francoisbest@gmail.com>
2018-02-11 09:29:48 +0100

    feat: Store JWT expiration date
    Will be key to checking if it needs pre-refreshing before each request

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,33 +1,50 @@
-const storeJwtToLocalStorage = (jwt = null) => {
+import jsonwebtoken from 'jsonwebtoken'
+
+const storeToLocalStorage = (jwt = null, exp = null) => {
   if (jwt === null) {
     window.localStorage.removeItem('stravels-jwt')
+    window.localStorage.removeItem('stravels-exp')
   } else {
     window.localStorage.setItem('stravels-jwt', jwt)
+    window.localStorage.setItem('stravels-exp', exp)
   }
 }
-const getJwtFromLocalStorage = () =>
-  window.localStorage.getItem('stravels-jwt') || null
+
+const getFromLocalStorage = () => ({
+  jwt: window.localStorage.getItem('stravels-jwt') || null,
+  exp: window.localStorage.getItem('stravels-exp') || null
+})
 
 const authState = {
-  jwt: null
+  jwt: null,
+  exp: null
 }
 
 // --
 
 export default {
   init: () => {
-    authState.jwt = getJwtFromLocalStorage()
+    const { jwt, exp } = getFromLocalStorage()
+    authState.jwt = jwt
+    authState.exp = exp ? parseFloat(exp) : null
   },
   authenticate: (jwt) => {
+    const { exp } = jsonwebtoken.decode(jwt)
     authState.jwt = jwt
-    storeJwtToLocalStorage(authState.jwt)
+    authState.exp = parseFloat(exp)
+    storeToLocalStorage(jwt, exp)
   },
   logout: () => {
     authState.jwt = null
-    storeJwtToLocalStorage(null)
+    authState.exp = null
+    storeToLocalStorage(null, null)
   },
   get jwt () {
     return authState.jwt
+  },
+  get isTokenValid () {
+    const now = Date.now() * 0.001
+    return authState.jwt !== null && authState.exp > now
   },
   get isAuthenticated () {
     return authState.jwt !== null
