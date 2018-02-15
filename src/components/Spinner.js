d22bccbd1a3a72f6c33ceea4d01ed46a0faf20df
Francois Best <francoisbest@gmail.com>
2018-02-15 23:24:50 +0100

    feat: Make Spinner customisable

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,4 +2,12 @@ import React from 'react'
 
 import './Spinner.css'
 
-export default () => <div className='spinner' />
+export default ({ size = 20, thickness = 3, backColor = '#eee', frontColor = 'limegreen' }) => (
+  <div className='spinner' style={{
+    width: size - thickness * 2,
+    height: size - thickness * 2,
+    borderColor: backColor,
+    borderTopColor: frontColor,
+    borderWidth: thickness
+  }} />
+)
