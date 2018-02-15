5699e4f07aaa0c699b0341ef560824041c5dc9ce
Francois Best <francoisbest@gmail.com>
2018-02-15 23:26:24 +0100

    Feat: make logo configurable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import logo from '../resources/stravels-logo.svg'
+import Logo from './Logo'
 import './Header.css'
 
 export default () => (
   <header className='app-header'>
-    <img src={logo} alt='logo' />
+    <Logo />
     <h1>Stravels</h1>
   </header>
 )
