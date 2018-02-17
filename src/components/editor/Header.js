f4eafe3fe3c586931b6497bfba9abb26299bcb1d
Francois Best <francoisbest@gmail.com>
2018-02-17 12:40:53 +0100

    feat: Left panel header wip

---
+++ src/components/editor/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Logo from '../Logo'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import profilePlaceholder from '../../resources/placeholder-user-64px.jpg'
+
+import './Header.css'
+
+const Header = ({ data }) => {
+  const profile = data.me ? data.me.profilePicture : profilePlaceholder
+  return (
+    <header className='editor-header'>
+      <Logo size={40} />
+      <h1>Stravels</h1>
+      <img src={profile} alt='profile' className='profile' />
+    </header>
+  )
+}
+
+const query = gql`
+query {
+  me {
+    profilePicture
+  }
+}
+`
+export default graphql(query)(Header)
---
+++ src/components/editor/LeftPanel.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import EditableText from '../core/EditableText'
+
+import './LeftPanel.css'
+
+const logTitleChange = (title) => {
+  console.log(title)
+}
+
+export default ({ onTitleChange = logTitleChange }) => (
+  <aside className='editor-left-panel'>
+    <Header />
+    <div className='editor-details'>
+      <EditableText className='title' onChange={onTitleChange} />
+      <div className='date-range'>July - August 2017</div>
+    </div>
+  </aside>
+)
