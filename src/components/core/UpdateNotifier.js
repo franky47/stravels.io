a050ad120fb4b75bf4519c76e62b15c22823ac2b
Francois Best <francoisbest@gmail.com>
2018-05-30 17:48:34 +0200

    refactor: Use Snackbar for update notifier

--- src/components/core/UpdateNotifier.js
+++ src/components/core/UpdateNotifier.js
@@ -1,15 +1,46 @@
-import React from 'react'
+// @flow
 
-import './UpdateNotifier.css'
+import * as React from 'react'
+import Media from 'react-media'
+
+// Material UI Components
+import Button from '@material-ui/core/Button'
+import Snackbar from '@material-ui/core/Snackbar'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
 
 const onClick = () => {
   // true = force reload from server, ignore cache
   window.location.reload(true)
 }
 
-export default () => (
-  <div className="update-notifier">
-    A new version of Stravels is available
-    <button onClick={onClick}>Refresh</button>
-  </div>
+type Props = {
+  +open: boolean
+}
+
+export default ({ open }: Props) => (
+  <Snackbar
+    open={open}
+    ContentProps={{ 'aria-describedby': 'message-id' }}
+    message={
+      <span id="message-id">
+        <Media query="(min-width: 400px)">
+          {(large: boolean) =>
+            large
+              ? 'A new version of Stravels is available'
+              : 'New version available'
+          }
+        </Media>
+      </span>
+    }
+    action={
+      <Button color="primary" size="small" onClick={onClick}>
+        Refresh
+      </Button>
+    }
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'left'
+    }}
+    autoHideDuration={null}
+  />
 )
